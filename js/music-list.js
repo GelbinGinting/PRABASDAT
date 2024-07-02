8e770751ffa021c245f37d10d99ce961fb5c53e6
GelbinGinting <164821325+GelbinGinting@users.noreply.github.com>
2024-07-02 22:35:27 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Teran Sora - Usman Ginting",
+    artist: "Usman ginting",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Biring Manggis – Lyodra Ginting",
+    artist: "Lyodra Ginting",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+      name: "Jambe Nurung Kerah - Plato Ginting",
+    artist: "Plato ginting",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Yang terlupakan - cover by Leo & Gelbin ",
+    artist: "Iwan Fals",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+      name: "Starboy - Peniel Ginting",
+    artist: "Peniel Ginting",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+      name: "Ada Dia Diantara Kita - Obbie Messakh ",
+      artist: "Obbie Messakh ",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
